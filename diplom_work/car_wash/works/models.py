f210cb26962a0b461417b8b421c2a024d7ce9f84
nikita <nedorezovnikita7@mail.ru>
2024-10-03 17:33:15 +0500

    first

--- diplom_work/car_wash/works/models.py
+++ diplom_work/car_wash/works/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Serv(models.Model):
+    time = models.CharField(max_length=5)
+    data = models.DateField()
+    instruction = models.CharField(max_length=200)
+
+
+class Work(models.Model):
+    price = models.IntegerField()
+    instruction = models.CharField(max_length=100)
+    time = models.TimeField(auto_now_add=True, blank=True)
+
--- diplom_work/car_wash/works/urls.py
+++ diplom_work/car_wash/works/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('authorization', views.authorization, name='authorization'),
     path('home_page', views.home_page, name='home-page'),
+    path('add_service', views.add_service, name='add-service'),
+    path('add_work', views.add_work, name='add-work'),
+    path('delete_service/<str:pk>/', views.delete_service, name="delete-service"),
+    path('update_service/<str:pk>/', views.update_service, name="update-service"),
+    path('result', views.result, name="result"),
+    path('reset', views.reset, name="reset"),
 ]
